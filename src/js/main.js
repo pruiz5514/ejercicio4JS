588bcff7395aa51d36e9db97a73f61bce810285e
pruiz5514 <pablo_ruiza@hotmail.com>
2024-04-18 13:23:30 -0500

    Se acaba

---
+++ src/js/main.js
@@ -0,0 +1,120 @@
+// ejercicio 1
+console.log("ejercicio 1");
+console.log(35>25);
+
+// ejercicio 2
+console.log("ejercicio 2");
+console.log(45<30);
+
+// ejercicio 3
+console.log("ejercicio 3");
+console.log(50 =="50");
+
+// ejercicio 4
+console.log("ejercicio 4");
+console.log(40 === 40);
+
+// ejercicio 5
+console.log("ejercicio 5");
+console.log("apple" != "orange");
+
+// ejercicio 6
+console.log("ejercicio 6");
+console.log("apple" !== "Apple");
+
+// ejercicio 7
+console.log("ejercicio 7");
+let a; 
+console.log(typeof(a));
+
+// ejercicio 8
+console.log("ejercicio 8");
+console.log(typeof(a));
+
+// ejercicio 9
+console.log("ejercicio 9");
+console.log(undefined == null);
+
+// ejercicio 10
+console.log("ejercicio 10");
+console.log(undefined === null);
+
+// ejercicio 11
+console.log("ejercicio 11");
+console.log(2.75>2.5);
+
+// ejercicio 12
+console.log("ejercicio 12");
+console.log(2.75>2.75);
+
+// ejercicio 13
+console.log("ejercicio 13");
+console.log(2.75>=2.75);
+
+// ejercicio 14
+console.log("ejercicio 14");
+console.log(2.75<=2.75);
+
+// ejercicio 15
+console.log("ejercicio 15");
+console.log(2.75>2.8);
+
+// ejercicio 16
+console.log("ejercicio 16");
+console.log(2.75<2.7);
+
+// ejercicio 17
+console.log("ejercicio 17");
+console.log("John" != 'jhon');
+
+// ejercicio 18
+console.log("ejercicio 18");
+console.log("John" != 'Jon');
+
+// ejercicio 19
+console.log("ejercicio 19");
+console.log(30 !== "30");
+
+// ejercicio 20
+console.log("ejercicio 20");
+console.log(25>>2);
+
+// ejercicio 21
+console.log("ejercicio 21");
+console.log(8<<3);
+
+// ejercicio 22
+console.log("ejercicio 22");
+console.log(15 == 15);
+
+// ejercicio 23
+console.log("ejercicio 23");
+console.log(20 !=30);
+
+// ejercicio 24
+console.log("ejercicio 24");
+console.log("15" > 30);
+
+// ejercicio 25
+console.log("ejercicio 25");
+console.log(20<40);
+
+// ejercicio 26
+console.log("ejercicio 26");
+console.log(100>="99");
+
+// ejercicio 27
+console.log("ejercicio 27");
+console.log(500 === "500");
+
+// ejercicio 28
+console.log("ejercicio 28");
+console.log(500 === "500");
+
+// ejercicio 29
+console.log("ejercicio 29");
+console.log(5 !== "5");
+
+// ejercicio 30
+console.log("ejercicio 30");
+console.log(3 >5 && 4<6);
